2282378b7391c9c72891aca88bbe03da4321e182
Daniel Axelrod <daxelrod@redhat.com>
2023-03-08 11:16:07 -0500

    Display the date and refresh every 10 seconds
    This will more easily demonstrate that the app is running.

--- server.js
+++ server.js
@@ -53,7 +53,8 @@ app.use(require('pino-http')({logger: pino}));
 app.get('/', (req, res) => {
   // Use req.log (a `pino` instance) to log JSON:
   req.log.info({message: 'Hello from Node.js Starter Application!'});
-  res.send('Hello from Node.js Starter Application!');
+  res.set('Refresh', 10);
+  res.send(`<html><head><title>Sample OpenShift App</title></head><body><h1>${new Date().toString()}</h1></body></html>`);
 });
 
 app.get('*', (req, res) => {
